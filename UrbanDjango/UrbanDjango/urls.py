c3660fe8b245fe0e2a28f3720a37a766f35dfd13
igor <igor_nu@bk.ru>
2025-01-14 05:40:36 +0500

    MVT. Вывод объектов в шаблоны

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path
+# from task1.views import *
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', index),
+# ]
 from django.urls import path
+from  task1.views import *
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('platform/', func_platform),
+    path('platform/games/', func_games),
+    path('platform/cart/', func_cart),
+    path('registration_html/',sign_up_by_html),
+    path('registration_class/',sign_up_by_django),
 ]
--- UrbanDjango/task1/views.py
+++ UrbanDjango/task1/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from .forms import UserRegister
+from .models import Buyer , Game
 
-# Create your views here.
+def  func_platform(request):
+   return  render(request,"platform.html")
+def  func_games(request):
+      list = []
+      games = Game.objects.all()
+      for g in games:
+          list.append(f' {g.title} | {g.description} Стоимость {g.cost}')
+      context = {'games':list}
+      return  render(request,"games.html",context)
+def  func_cart(request):
+     return  render(request,"cart.html")
+
+
+users =['lom','istr','jem','kevi','link']
+info = {}
+def  sign_up_by_html(request):
+     if request.method == 'POST':
+        username= request.POST.get("username")
+        password = request.POST.get('password')
+        repeat_password= request.POST.get('repeat_password')
+        age = request.POST.get('age')
+        buyers = Buyer.objects.all()
+        name_in = False
+        for i in buyers:
+             if i.name.upper() == username.upper():
+                name_in = True
+                break
+        if name_in == False and password==repeat_password :
+
+            Buyer.objects.create(name=username, balance=0, age=age)
+            return render(request, 'registration_page.html', {'username':f'Приветствуем,{username} !'})
+        else:
+           if password != repeat_password : info.update({'error':'Пароли не совпадают'})
+           if name_in == True: info.update({'error':'Пользователь уже существует'})
+           return render(request, 'registration_page.html',info)
+     return  render(request,'registration_page.html')
+
+
+def  sign_up_by_django(request):
+     if request.method == 'POST':
+         form = UserRegister(request.POST)
+         if form.is_valid():
+             username = form.cleaned_data["username"]
+             password = form.cleaned_data['password']
+             repeat_password = form.cleaned_data['repeat_password']
+             age = form.cleaned_data['age']
+             buyers = Buyer.objects.all()
+             name_in = False
+             for i in buyers:
+                 if i.name.upper() == username.upper():
+                     name_in = True
+                     break
+             if name_in == False and password == repeat_password:
+                 Buyer.objects.create(name=username, balance=0, age=age)
+                 return render(request, 'registration_page.html', {'username': f'Приветствуем,{username} !'})
+             else:
+                 if password != repeat_password: info.update({'error': 'Пароли не совпадают'})
+                 if name_in == True: info.update({'error': 'Пользователь уже существует'})
+                 return render(request, 'registration_page.html', info)
+
+     else:
+         form = UserRegister()
+     return render(request, 'registration_page.html',{'form':form})
+
