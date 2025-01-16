916fd8dab604c04d628d433678904b10bd330cd1
igor <igor_nu@bk.ru>
2025-01-17 02:02:51 +0500

    Настраиваем СУБД postgre в django

--- UrbanDjango/UrbanDjango/settings.py
+++ UrbanDjango/UrbanDjango/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'UrbanDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop_db',  # Имя базы данных
+        'USER': 'postgres',  # Пользователь PostgreSQL
+        'PASSWORD': 'admin',  # Пароль PostgreSQL
+        'HOST': '127.0.0.1',  # Адрес сервера базы данных
+        'PORT': '5432',  # Порт подключени
+       }
 }
 
 
